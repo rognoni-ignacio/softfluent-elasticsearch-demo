80f300e2858ef73789966aa5e3eb57f98d762db3
Ignacio ROGNONI <rognoni.ignacio@gmail.com>
2019-09-13 00:03:24 +0200

    Add Search all documents for high level and low level clients

--- sf.elasticsearch.demo/Controllers/HomeController.cs
+++ sf.elasticsearch.demo/Controllers/HomeController.cs
@@ -64,6 +64,29 @@ namespace sf.elasticsearch.demo.Controllers
             return Ok(responseJson);
         }
 
+        public IActionResult LowLevelSearchAll()
+        {
+            // Connecting using additional settings
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200/"))
+    .RequestTimeout(TimeSpan.FromMinutes(2));
+
+            var lowlevelClient = new ElasticLowLevelClient(settings);
+
+            var searchResponse = lowlevelClient.Search<StringResponse>("people", PostData.Serializable(new
+            {
+                from = 0,
+                size = 10,
+                query = new
+                {
+                    match_all = new {}
+                }
+            }));
+
+            var responseJson = searchResponse.Body;
+
+            return Ok(responseJson);
+        }
+
         public async System.Threading.Tasks.Task<IActionResult> HighLevelNewIndexAsync()
         {
             // Connecting using additional settings
@@ -94,6 +117,24 @@ namespace sf.elasticsearch.demo.Controllers
                         .Field(f => f.FirstName)
                         .Query(name)
                         )
+                )
+            );
+
+            var people = searchResponse.Documents;
+
+            return Ok(people);
+        }
+
+        public IActionResult HighLevelSearchAll()
+        {
+            // Connecting using additional settings
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("people");
+
+            var client = new ElasticClient(settings);
+
+            var searchResponse = client.Search<Person>(s => s
+                .Query(q => q
+                    .MatchAll()
                 )
             );
 
